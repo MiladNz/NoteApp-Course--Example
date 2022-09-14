65c471937b7d69e9438b4e822bc957de87b01cba
MiladNz <milad.n91@gmail.com>
2022-09-15 02:30:11 +0430

    savedNote-deleteNote

--- js/main.js
+++ js/main.js
@@ -21,13 +21,38 @@ const notes = [
 
 class NotesAPI {
   static getAllNotes() {
-    const savedNotes = notes || []; //JSON.parse(localStorage.getItem("notes-app"))
+    const savedNotes = JSON.parse(localStorage.getItem("notes-app")) || []; //notes or JSON.parse(localStorage.getItem("notes-app"))
     return savedNotes.sort((a, b) => {
       return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
     });
   }
-  saveNote() {}
-  deleteNote() {}
+  static saveNote(noteToSave) {
+    const notes = NotesAPI.getAllNotes(noteToSave);
+    const existedNote = notes.find((n) => n.id == noteToSave.id);
+    if (existedNote) {
+      existedNote.title = noteToSave.title;
+      existedNote.body = noteToSave.body;
+      existedNote.updated = new Date().toISOString;
+    } else {
+      noteToSave.id = new Date().getTime;
+      noteToSave.updated = new Date().toISOString;
+      notes.push(noteToSave);
+    }
+    localStorage.setItem("notes-app", JSON.stringify(notes));
+  }
+  static deleteNote(id) {
+    const notes = NotesAPI.getAllNotes();
+    const filteredNotes = notes.filter((n) => n.id != id);
+    localStorage.setItem("notes-app", JSON.stringify(filteredNotes));
+  }
 }
 
-console.log(NotesAPI.getAllNotes());
+// console.log(
+//   NotesAPI.saveNote({
+//     id: 1,
+//     title: "first edited note",
+//     body: "this is first edited note",
+//     updated: "2021-10-31T15:02:00.411Z",
+//   })
+// );
+console.log(NotesAPI.deleteNote(3));
