ffcd76aefc4b4be84a83ffc8e82ca2c3e5a435ab
MiladNz <milad.n91@gmail.com>
2022-09-18 01:26:15 +0430

    NoteView class

--- js/main.js
+++ js/main.js
@@ -2,4 +2,11 @@ import NotesAPI from "./NotesAPI.js";
 import NotesView from "./NotesView.js";
 
 const app = document.getElementById("app");
-const view = NotesView(app);
+const view = new NotesView(app, {
+  onNoteAdd() {
+    console.log("note has been added.");
+  },
+  onNoteEdit(newTitle, newBody) {
+    console.log(newTitle, newBody);
+  },
+});
